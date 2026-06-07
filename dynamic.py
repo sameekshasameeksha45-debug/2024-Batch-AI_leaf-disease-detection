32f2102b849c2c9005650fae523093ee25e7bb6d
SAMEEKSHA S <sameekshasameeksha45@gmail.com>
2026-06-07 10:31:53 +0530

    Delete dynamic.py

--- dynamic.py
+++
@@ -1,9 +0,0 @@
-import os
-
-UPLOAD_FOLDER = 'static'
-
-if not os.path.exists(UPLOAD_FOLDER):
-    os.makedirs(UPLOAD_FOLDER)
-
-filepath = os.path.join(UPLOAD_FOLDER, 'temp.jpg')
-file.save(filepath)
